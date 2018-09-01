35096e83fa83cd43e0b58239094666f4c7fdc0bb
Quang Mai <quangmai09@gmail.com>
2018-09-01 01:58:46 -0500

    add two sql projects

---
+++ SqlConnection/SqlConnection/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SqlConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Debug.WriteLine("Hello world");
+
+
+            var connectionString = "Data Source=DESKTOP-TDB8N4J;Initial Catalog=AdventureWorks2014;Integrated Security=True";
+
+            var query = @"SELECT TOP 10 [AddressID]
+                          ,[AddressLine1]
+                          ,[AddressLine2]
+                          ,[City]
+                          ,[StateProvinceID]
+                          ,[PostalCode]
+                          ,[SpatialLocation]
+                          ,[rowguid]
+                          ,[ModifiedDate]
+                        FROM [AdventureWorks2014].[Person].[Address]";
+
+
+
+
+        }
+    }
+}
