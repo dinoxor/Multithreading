f62751941f4fb5516d41f5376e2b31fab7e2de6d
Quang Mai <quangmai09@gmail.com>
2018-09-04 00:22:06 -0500

    Add solution to problem 2 without threading

--- Multithreading/Multithreading/Project Euler/HelperService.cs
+++ Multithreading/Multithreading/Project Euler/HelperService.cs
@@ -6,9 +6,65 @@ namespace Multithreading.Project_Euler
 {
     public class HelperService
     {
-        public List<int> GetFibonacciSequenceByTerm (int term)
+        public List<int> GetFibonacciSequenceByTerm(int targetTerm)
         {
+            var fibSequences = new List<int> { 1, 2 };
 
+            if (targetTerm == 1)
+            {
+                return new List<int> { 1 };
+            }
+            else if (targetTerm == 2)
+            {
+                return new List<int> { 1, 2 };
+            }
+            else
+            {
+                var fibValue1 = 1;
+                var fibValue2 = 2;
+
+                for (int term = 3; term <= targetTerm; term++)
+                {
+                    var newFibValue = fibValue1 + fibValue2;
+                    fibValue1 = fibValue2;
+                    fibValue2 = newFibValue;
+
+                    fibSequences.Add(newFibValue);
+                }
+            }
+
+            return fibSequences;
+        }
+
+        public List<int> GetFibonacciSequenceByLessThanOrEqualToValue(int value)
+        {
+            var fibSequences = new List<int> { 1, 2 };
+
+            var fibValue1 = 1;
+            var fibValue2 = 2;
+            var newFibValue = 0;
+
+            var continueFib = true;
+
+            do
+            {
+                newFibValue = fibValue1 + fibValue2;
+
+                if (newFibValue <= value)
+                {
+                    fibValue1 = fibValue2;
+                    fibValue2 = newFibValue;
+
+                    fibSequences.Add(newFibValue);
+                }
+                else
+                {
+                    continueFib = false;
+                }
+
+            } while (continueFib);
+
+            return fibSequences;
         }
     }
 }
--- Multithreading/Multithreading/Project Euler/Problem2.cs
+++ Multithreading/Multithreading/Project Euler/Problem2.cs
@@ -2,26 +2,48 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Diagnostics;
+using System.Linq;
 
 namespace Multithreading.Project_Euler
 {
     public class Problem2
     {
+        //By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+
         private HelperService _helperService;
 
         public Problem2 (HelperService helperService)
         {
             _helperService = helperService;
         }
+
         public void Run()
         {
-            var fibSequences = _helperService.GetFibonacciSequenceByTerm(10);
+            var fibSequences = _helperService.GetFibonacciSequenceByLessThanOrEqualToValue(4000001);
 
             foreach (var fibValue in fibSequences)
             {
                 Debug.WriteLine(fibValue);
             }
 
+            DisplayEvenSumFibonacci(fibSequences);
         }
+
+        private void DisplayEvenSumFibonacci(List<int> fibSequences)
+        {
+            var evenFibSequences = fibSequences.Where(x => x % 2 == 0);
+
+            Debug.WriteLine("\nEven");
+            var evenSum = 0;
+
+            foreach (var fibValue in evenFibSequences)
+            {
+                evenSum += fibValue;
+                Debug.WriteLine(fibValue);
+            }
+
+            Debug.WriteLine($"\nSum of even Fibonacci Sequence less than four million: {evenSum}");
+        }
+
     }
 }
