920114adc4be6e3db662d0265d38bf2f08da63fb
Quang Mai <quangmai09@gmail.com>
2018-10-08 01:07:33 -0500

    Modified code to run as threads

--- RestWebMatrix/RestWebMatrix/Program.cs
+++ RestWebMatrix/RestWebMatrix/Program.cs
@@ -3,120 +3,200 @@ using System.Net.Http;
 using System.Diagnostics;
 using System.Linq;
 using Newtonsoft.Json;
+using System.Threading;
+using System.Collections.Generic;
 
 namespace RestWebMatrix
 {
     public class Program
     {
+        private static List<string> _appIds = new List<string> { "496036468", "745006435" };
+        private static int _currentAppIdIndex = 0;
+        private const string _urlParameter = "?app_id=";        
+        private static string _currentAppId = string.Empty;
+
+        private static HttpClient _lowPriorityClient;
+        private static HttpClient _highPriorityClient;
+        private static HttpClient _resetClient;
+
+        private static List<Request> _lowPriorityRequests = new List<Request>();
+        private static List<Request> _highPriorityRequests = new List<Request>();
+
         static void Main(string[] args)
         {
-            //http://ec2-54-241-228-71.us-west-1.compute.amazonaws.com:3000/low_priority?app_id=496036468
+            //Low priority requests will execute in order it was received
+            //After 20 seconds, 100 high priority requests will execute 
+            //Remaining low priority request will run for an additional 20 seconds before another 20 high priority requests will execute
+            
+            Initialize();                     
 
-            const string appId1 = "496036468";
-            const string appId2 = "745006435";
+            //reset both App Ids 
+            foreach (var appIds in _appIds)
+            {
+                _resetClient.GetAsync($"{_urlParameter}{appIds}");
+            }            
+
+            Thread lowPriorityThread = new Thread(new ThreadStart(QueueLowPriority));
+            Thread highPriorityThread = new Thread(new ThreadStart(QueueHighPriority));
+            Thread executeThread = new Thread(new ThreadStart(ExecuteRequests));
+
+            lowPriorityThread.Start();
+            executeThread.Start();
+
+            Thread.Sleep(20000);
+            highPriorityThread.Start();
+        }
+
+        private static void Initialize()
+        {          
             const string lowPriorityRoute = "low_priority";
             const string highPriorityRoute = "high_priority";
             const string resetRoute = "reset";
             const string baseUrl = @"http://ec2-54-241-228-71.us-west-1.compute.amazonaws.com:3000/";
-            const string urlParameter = "?app_id=";
-
+            
             var lowPriorityUrl = $"{baseUrl}{lowPriorityRoute}";
             var highPriorityUrl = $"{baseUrl}{highPriorityRoute}";
             var resetUrl = $"{baseUrl}{resetRoute}";
 
-            var lowPriorityClient = getClient(lowPriorityUrl);
-            var highPriorityClient = getClient(highPriorityUrl);
-            var resetClient = getClient(resetUrl);
+            _lowPriorityClient = GetClient(lowPriorityUrl);
+            _highPriorityClient = GetClient(highPriorityUrl);
+            _resetClient = GetClient(resetUrl);
 
-            //reset            
-            resetClient.GetAsync($"{urlParameter}{appId1}");
-            resetClient.GetAsync($"{urlParameter}{appId2}");            
+            _currentAppId = _appIds[_currentAppIdIndex];
+        }
 
-            var useDefaultAppId = true;
-            var shouldSwapDefaultAppId = true;
-            
-
-            for (int apiCallCounter = 0; apiCallCounter < 850; apiCallCounter++)
+        private static void QueueLowPriority()
+        {
+            for (int i = 0; i < 750; i++)
             {
-                HttpResponseMessage response;
-                var currentAppId = useDefaultAppId ? appId1 : appId2;
-                var priority = string.Empty;
-
-                if (apiCallCounter >= 325 && apiCallCounter <=425)
+                var request = new Request
                 {
-                    priority = "high";
-                    response = executeApiCall(highPriorityClient, urlParameter, currentAppId);
+                    Client = _lowPriorityClient,
+                    Url = _urlParameter
+                };
+
+                _lowPriorityRequests.Add(request);
+            }
+        }
+
+        private static void QueueHighPriority()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                var request = new Request
+                {
+                    Client = _highPriorityClient,
+                    Url = _urlParameter
+                };
+
+                _highPriorityRequests.Add(request);
+            }
+
+            Thread.Sleep(20000);
+
+            for (int i = 0; i < 10; i++)
+            {
+                var request = new Request
+                {
+                    Client = _highPriorityClient,
+                    Url = _urlParameter
+                };
+
+                _highPriorityRequests.Add(request);
+            }
+        }
+
+        private static void ExecuteRequests()
+        {
+            //Executes in FIFO queue
+            var counter = 0;
+            var highCounter = 0;
+
+            while (_lowPriorityRequests.Count > 0 || _highPriorityRequests.Count > 0)
+            {                
+                var priority = string.Empty;
+                //Request request = null;
+
+                List<Request> request = new List<Request>();
+
+                //Higher priority will have execution precedences 
+                if (_highPriorityRequests.Count > 0)
+                {
+                    //TO DO: may need to lock due to race condition
+                    priority = $"high:{++highCounter}";
+
+                    request = _highPriorityRequests;//pass by reference
                 }
                 else
                 {
                     priority = "low";
-                    response = executeApiCall(lowPriorityClient, urlParameter, currentAppId);
-                }               
-                
-                var responseUsageHeader = getUsageHeader(response); 
-                if (responseUsageHeader != null)
-                {
-                    if (shouldSwapDefaultAppId || responseUsageHeader.call_count > 50 || responseUsageHeader.total_cputime > 50 || responseUsageHeader.total_time > 50)
-                    {
-                        //swap app Id 
-                        useDefaultAppId = !useDefaultAppId;
-                    }
-                }
-                else
-                {
-                    //app id reached its limit
-                    shouldSwapDefaultAppId = false;
-                    useDefaultAppId = !useDefaultAppId;
+
+                    request = _lowPriorityRequests;
                 }
 
+                var firstRequest = request[0];
 
-                if (response.IsSuccessStatusCode)
-                {               
-                    Debug.WriteLine($"{apiCallCounter} \t[{currentAppId}] \t[{priority}]: {response.StatusCode}\t call_count: {responseUsageHeader.call_count}");
-                }                       
+                var response = firstRequest.Client.GetAsync($"{firstRequest.Url}{_currentAppId}").Result;
+                var responseUsageHeader = GetUsageHeader(response);
+                LoadBalanceAppId(responseUsageHeader);
 
-            }            
-        }
-
-        private static void callLowPriority()
-        {
-            for (int i = 0; i < 750; i++)
-            {
-
+                if (responseUsageHeader != null)//if successful
+                {
+                    counter++;
+                    Debug.WriteLine($"{counter}\t[{_currentAppId}] \t[{priority}]: {response.StatusCode}\t call_count: {responseUsageHeader.call_count}");
+                    request.RemoveAt(0);
+                }                                  
             }
-
         }
 
-        private static void callHighPriority()
+        private static void LoadBalanceAppId(UsageHeader responseUsageHeader)
         {
-            //sleep for 15 seconds
-
-            for (int i =0; i< 100; i++)
+            if (responseUsageHeader != null)
             {
-
+                if (responseUsageHeader.call_count > 50 || responseUsageHeader.total_cputime > 50 || responseUsageHeader.total_time > 50)
+                {
+                    _currentAppId = GetNextAvailableAppId(_currentAppIdIndex);
+                }
             }
-
-
+            else
+            {
+                //app id reached its limit and throws an error
+                _currentAppId = GetNextAvailableAppId(_currentAppIdIndex);
+            }
         }
 
-        private static UsageHeader getUsageHeader (HttpResponseMessage response)
+        private static string GetNextAvailableAppId(int currentAppIdIndex)
+        {
+            var maxIndex = _appIds.Count-1;
+            currentAppIdIndex++;
+
+            //if last index, get the first index
+            if (currentAppIdIndex > maxIndex)
+            {
+                _currentAppIdIndex = 0;
+                return _appIds[_currentAppIdIndex];
+            }
+            else
+            {
+                //get the next index
+                _currentAppIdIndex = currentAppIdIndex;
+                return _appIds[_currentAppIdIndex];
+            }
+        }
+
+        private static UsageHeader GetUsageHeader(HttpResponseMessage response)
         {
             if (response.IsSuccessStatusCode)
             {
+                //extract and deserialize Usage header
                 var xAppUsageResponseJson = response.Headers.GetValues("X-App-Usage").FirstOrDefault();
                 return JsonConvert.DeserializeObject<UsageHeader>(xAppUsageResponseJson);
             }
 
             return null;
-            
-        }
+        }       
 
-        private static HttpResponseMessage executeApiCall(HttpClient client, string url, string appId)
-        {
-            return client.GetAsync($"{url}{appId}").Result;
-        }
-
-        private static HttpClient getClient(string baseAddress)
+        private static HttpClient GetClient(string baseAddress)
         {
             var client = new HttpClient();
 
@@ -127,7 +207,7 @@ namespace RestWebMatrix
 
             return client;
         }
-        
+
         public class UsageHeader
         {
             public int call_count { get; set; }
@@ -135,5 +215,10 @@ namespace RestWebMatrix
             public int total_cputime { get; set; }
         }
 
+        public class Request
+        {
+            public HttpClient Client { get; set; }
+            public string Url { get; set; }
+        }
     }
 }
