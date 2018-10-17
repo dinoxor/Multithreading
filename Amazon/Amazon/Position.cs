794b35379bef6b6bba15ec799b5b09acaa5306e3
Quang Mai <quangmai09@gmail.com>
2018-10-17 00:35:51 -0500

    Add Amazon practice

---
+++ Amazon/Amazon/Position.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Amazon
+{
+    public class Position
+    {
+        private int[,] _area;
+        private int _numRows;
+        private int _numColumns;
+        private int[] _currentPosition;
+
+        private int[] _nextRightPosition;
+        private int[] _nextDownPosition;
+        private int[] _nextUpPosition;
+        private int[] _nextLeftPosition;
+
+        public int? NextRightPositionResult;
+        public int? NextDownPositionResult;
+        public int? NextUpPositionResult;
+        public int? NextLeftPositionResult;
+
+        public bool CanGoRight;
+        public bool CanGoDown;
+        public bool CanGoUp;
+        public bool CanGoLeft;
+
+        public Position(int[,] area, int[] currentPosition)
+        {
+            _area = area;
+            _numRows = area.GetLength(0);
+            _numColumns = area.GetLength(1);
+            _currentPosition = currentPosition;
+
+            _nextRightPosition = GetNextRightPosition();
+            _nextDownPosition = GetNextDownPosition();
+            _nextUpPosition = GetNextUpPosition();
+            _nextLeftPosition = GetNextLeftPosition();
+
+            CanGoRight = DetermineRightPositionIsValid();
+            CanGoDown = DetermineDownPositionIsValid();
+            CanGoUp = DetermineUpPositionIsValid();
+            CanGoLeft = DetermineLeftPositionIsValid();
+            
+        }
+
+        private void SetRightPositionProperties()
+        {
+            //(x+1 , y)
+            _nextRightPosition = new int[] { _currentPosition[0] + 1, _currentPosition[1] };
+
+            //if x axis is less than number of rows
+            //AND area(x+1 , y) does not equal 0
+            CanGoRight = _currentPosition[0] < _numRows && _area[_nextRightPosition[0], _nextRightPosition[1]] != 0 ? true : false;
+            
+
+
+        }
+
+        private void SetDownPositionProperties()
+        {
+            _nextDownPosition = new int[] { _currentPosition[0], _currentPosition[1] + 1 };
+        }
+
+        private void SetUpPositionProperties()
+        {
+
+        }
+
+        private void SetLeftPositionProperties()
+        {
+
+        }
+
+        private int[] GetNextDownPosition ()
+        {
+            //(x , y+1)
+            return new int[] { _currentPosition[0], _currentPosition[1] + 1 };
+        }
+        private int[] GetNextUpPosition ()
+        {
+            //(x , y-1)
+            return new int[] { _currentPosition[0], _currentPosition[1] - 1 }; 
+        }
+        private int[] GetNextLeftPosition ()
+        {
+            //(x-1 , y)
+           return new int[] { _currentPosition[0] - 1 , _currentPosition[1] }; 
+        }
+
+             
+        public bool DetermineDownPositionIsValid()
+        {
+            //if y axis is less than number of columns
+            //AND area(x,y+1) does not equal 0
+            return _currentPosition[1] < _numColumns && _area[_nextDownPosition[0], _nextDownPosition[1]] != 0 ? true : false;
+        }
+        public bool DetermineUpPositionIsValid()
+        {
+            return _currentPosition[1] >= 0 && _area[_nextUpPosition[0], _nextUpPosition[1]] != 0 ? true : false;
+        }
+        public bool DetermineLeftPositionIsValid()
+        {
+            //if x axis is greater than or equal to 0
+            //AND area(x-1, y) != 0
+            return _currentPosition[0] >= 0 && _area[_nextLeftPosition[0], _nextLeftPosition[1]] != 0 ? true : false;
+        }
+
+
+    }
+
+}
---
+++ Amazon/Amazon/Program.cs
@@ -0,0 +1,288 @@
+﻿using System.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Amazon
+{
+    public class Program
+    {
+        private static int _numRows;
+        private static int _numColumns;
+        private List<int> _totalDistances = new List<int>();
+
+        static void Main(string[] args)
+        {
+            test2();
+        }
+
+        static void test2()
+        {           
+            _numRows = 5;
+            _numColumns = 2;
+            int[,] area = { { 1, 1, 1, 1, 0 },
+                            { 1, 1, 1, 9, 0 } };
+            int[] position = { 0, 0 };
+
+            //Debug.WriteLine(area[0, 1]); // 1 
+            //Debug.WriteLine(area[0, 4]); // 9
+            //Debug.
+
+            var total = Right(area, position);
+
+            Debug.WriteLine(total);
+        }
+
+        //static private int [] GetRightPosition(int[] currentPosition)
+        //{
+        //    return new int[] { currentPosition[0], currentPosition[1] + 1 };
+        //}
+
+        //static private int[] GetUpPosition(int[] currentPosition)
+        //{
+        //    return new int[] { currentPosition[0] -1, currentPosition[1] };
+        //}
+
+        //static private int[] GetDownPosition(int[] currentPosition)
+        //{
+        //    return new int[] { currentPosition[0] + 1, currentPosition[1] };
+        //}
+
+        //static private int[] GetLeftPosition(int[] currentPosition)
+        //{
+        //    return new int[] { currentPosition[0], currentPosition[1] - 1 };
+        //}
+
+        //static private bool CanGoRight(int[] position)
+        //{
+        //    return position[1] < _numRows ? true : false;
+        //}
+        //static private bool CanGoDown (int[] position)
+        //{
+        //    return position[0] < _numColumns ? true : false;
+        //}
+        //static private bool CanGoUp (int[] position)
+        //{
+        //    return position[0] >= 0 ? true : false;
+        //}
+        //static private bool CanGoLeft (int[] position)
+        //{
+        //    return position[1] >= 0 ? true : false;
+        //}
+
+        static private int Right(int[,] area, int[] currentPosition)
+        {
+            var position = new Position(area, currentPosition);
+
+            var isDeadEnd = (  !position.CanGoRight 
+                            && !position.CanGoDown 
+                            && !position.CanGoUp);
+
+            var next_right = GetRightPosition(currentPosition);
+            var next_up = GetUpPosition(currentPosition);
+            var next_down = GetDownPosition(currentPosition);
+
+            if (isDeadEnd)
+            {
+                return -100;
+            }
+
+            if (CanGoRight(currentPosition))
+            {
+                if (area[next_right[0], next_right[1]] == 9)
+                {
+                    return 1;
+                }
+
+                if (area[next_right[0], next_right[1]] == 1)
+                {
+                    return 1 + Right(area, next_right);
+                }                     
+
+                //if (currentPosition[0] == 0 && currentPosition[1] == 0)// if (0,0)
+                //{
+                //    _totalDistances.Add();
+                //}
+            }
+
+            if (CanGoDown(currentPosition))
+            {
+                if (area[next_down[0], next_down[1]] == 9)
+                {
+                    return 1;
+                }
+
+                if (area[next_down[0], next_down[1]] == 1)
+                {
+                    return 1 + Down(area,next_down);
+                }
+
+                if (area[next_right[0], next_right[1]] == 0 && isDeadEnd)
+                {
+                    return -100;
+                }
+            }
+
+            return -1000;
+        }
+
+
+
+        static private int Down(int[,] area, int[] currentPosition)
+        {
+            var isDeadEnd = (!CanGoRight(currentPosition) && !CanGoDown(currentPosition) && !CanGoLeft(currentPosition));
+
+            var next_right = GetRightPosition(currentPosition);
+            var next_left = GetLeftPosition(currentPosition);
+            var next_down = GetDownPosition(currentPosition);
+
+
+
+            return 1;
+        }
+
+        static private int Up(int[,] area, int current_X, int current_Y)
+        {
+            return 1;
+        }
+
+        static private int Left(int[,] area, int current_X, int current_Y)
+        {
+            return 1;
+        }
+
+
+
+        static void test1()
+        {
+            int numDestinations = 4;
+            int[,] allLocations = { { 1, 2, 3 }, { 3, 4, 4 }, { 5, 6, 5 }, { 7, 8, 5 } };
+            int numDeliveries = 2;
+
+            double[,,] calculatedDistanceWithLocation = new double[numDestinations, 3, 1]; //[x,y,distance]
+            List<double> distances = new List<double>();
+
+
+            List<List<int>> allOrderedCoordinates = new List<List<int>>();
+
+
+
+
+            //Debug.WriteLine(allLocations.GetLength(0)); // 4: rows
+            //Debug.WriteLine(allLocations.GetLength(1)); // 2: columns
+            //Debug.WriteLine($"{allLocations[0, 0]} {allLocations[0,1]}"); // first row, column 1 and 2
+
+            for (int locationCoordinate = 0; locationCoordinate < allLocations.GetLength(0); locationCoordinate++)
+            {
+                double x = allLocations[locationCoordinate, 0];
+                double y = allLocations[locationCoordinate, 1];
+                double distance = Math.Round(Math.Sqrt((x * x) + (y * y)), 2);
+
+                //Debug.WriteLine(distance);
+
+
+                calculatedDistanceWithLocation[locationCoordinate, 0, 0] = x; //set x coordinate
+                calculatedDistanceWithLocation[locationCoordinate, 1, 0] = y; //set y coordinate
+                calculatedDistanceWithLocation[locationCoordinate, 2, 0] = distance;
+
+                distances.Add(distance);
+            }
+
+            distances.Sort();
+
+            foreach (var distance in distances)
+            {
+                Debug.WriteLine(distance);
+                List<int> resultCoordinate = new List<int>();
+
+                for (int row = 0; row < allLocations.GetLength(0); row++)
+                {
+                    var targetDistance = calculatedDistanceWithLocation[row, 2, 0];
+
+                    if (distance == targetDistance)
+                    {
+                        Debug.WriteLine($"{calculatedDistanceWithLocation[row, 0, 0]},{calculatedDistanceWithLocation[row, 1, 0]}");
+                        resultCoordinate.Add(Convert.ToInt32(calculatedDistanceWithLocation[row, 0, 0]));
+                        resultCoordinate.Add(Convert.ToInt32(calculatedDistanceWithLocation[row, 1, 0]));
+                        allOrderedCoordinates.Add(resultCoordinate);
+
+                        break;
+                    }
+                }
+            }
+
+            //foreach (var coord in allOrderedCoordinates)
+            //{
+            //    Debug.WriteLine(coord[0] + " " + coord[1]);
+            //}
+
+
+
+
+            //foreach (var location in calculatedDistanceWithLocation)
+            //{
+            //    Debug.WriteLine(location);
+            //}
+
+
+        }
+
+        static void demo1()
+        {
+            //test case 1
+            //int[] states = { 1, 0, 0, 0, 0, 1, 0, 0 };
+            //int days = 1;
+
+            //test case 2
+            int[] states = { 1, 1, 1, 0, 1, 1, 1, 1 };
+            int days = 2;
+
+            int[] newStates = new int[8];
+
+            for (int currentDay = 1; currentDay <= days; currentDay++)
+            {
+                for (int i = 0; i < states.Length; i++)
+                {
+                    int previousNeighbor;
+                    int nextNeighbor;
+
+                    if (i == 0) //if beginning
+                    {
+                        previousNeighbor = 0;
+                        nextNeighbor = states[i + 1];
+                    }
+                    else if (i == states.Length - 1) //if beginning or ending
+                    {
+                        previousNeighbor = states[i - 1];
+                        nextNeighbor = 0;
+                    }
+                    else
+                    {
+                        previousNeighbor = states[i - 1];
+                        nextNeighbor = states[i + 1];
+                    }
+
+                    if (previousNeighbor == nextNeighbor)
+                    {
+                        newStates[i] = 0;
+                    }
+                    else
+                    {
+                        newStates[i] = 1;
+                    }
+
+                }
+
+                foreach (var state in newStates)
+                {
+                    Debug.Write(state);
+                }
+
+                Debug.WriteLine("");
+
+                Array.Copy(newStates, states, states.Length);
+
+            }
+        }
+    }
+}
